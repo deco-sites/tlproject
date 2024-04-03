55b19e34750d10a36c25f9847664cbc16f166fdf
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-04-03 19:07:26 -0300

    feat: timeline items insertion feature

--- sections/TimeLine.tsx
+++ sections/TimeLine.tsx
@@ -1,3 +1,5 @@
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
 
 interface Props {
   /**
@@ -8,21 +10,38 @@ interface Props {
   * @description The description of color.
   */
   color?: string;
+  timelineItems: TimelineItem[];
 }
 
-export default function Section({ name = "Capy", color = "green" }: Props) {
+interface TimelineItem {
+  name: string;
+  time: string;
+  image?: ImageWidget;
+}
+
+export default function Section({ name = "Capy", color = "green", timelineItems }: Props) {
+
+  const colorVariants = {
+    blue: 'bg-red-200',
+    red: 'bg-blue-200',
+  }
+
   return <div class={`bg-${color}-200 p-8 rounded-md shadow-md`}>
-    <h2 class="text-2xl font-semibold mb-4">Fancy Component</h2>
-
-    
-    <div class="space-y-4">
-      <button class={`btn btn-${color}`}>Click me</button>
-
-      <input type="text" class="input input-bordered" placeholder="Type something" />
-
-        <div class={`alert alert-${color}`}>
-          {name}
+    <h2 class="text-2xl font-semibold mb-4">{name}</h2>
+    {
+      timelineItems.map(i => (
+        <div class="flex items-center justify-center mb-3">
+          <span class="inline-block w-40 bg-blue-300">{i.time}</span>
+          <Image
+            class="ml-3 mr-3 bg-red-300"
+            src={i.image || ""}
+            width={100}
+          />
+          <p class="bg-green-300 w-[30vw]">
+            {i.name}
+          </p>
         </div>
-    </div>
+      ))
+    }
   </div>
 }
